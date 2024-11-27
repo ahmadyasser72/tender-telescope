98ae9b2a27eeac7c5d66836ece4aee3a8023b67d
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-11-27 22:35:29 +0800

    tambah adapter cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   redirects: {
     "/": "/start",
   },
+
   integrations: [svelte(), tailwind({ applyBaseStyles: false })],
+  adapter: cloudflare(),
 });
