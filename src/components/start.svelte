e9e0b6405fdc8d155e71d26dbd7e4aba7b9fca51
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-11-28 13:39:16 +0800

    fix build error

---
+++ src/components/start.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="mt-12 flex flex-col space-y-4">
+  <Button href="/start-settings" size="big">Mulai</Button>
+  <Button id="close-button" size="big" variant="outline">Keluar</Button>
+</div>
