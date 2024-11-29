fca74b469a0f7193a8cb06fb9ae0c4f60524980d
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-11-29 09:21:17 +0800

    refactor manajemen cookie untuk pengaturan game

--- src/actions/game.ts
+++ src/actions/game.ts
@@ -1,5 +1,9 @@
-import { defineAction } from "astro:actions";
+import type { Difficulty } from "$lib/types";
+import { ActionError, defineAction } from "astro:actions";
 import { z } from "astro:schema";
+
+const difficultyCookie = "game-difficulty";
+const langaugesCookie = "game-languages";
 
 export const game = {
   initialize: defineAction({
@@ -8,14 +12,39 @@ export const game = {
       languages: z.array(z.string()),
     }),
     handler: ({ difficulty, languages }, ctx) => {
-      ctx.cookies.set("game-difficulty", difficulty, {
+      ctx.cookies.set(difficultyCookie, difficulty, {
         httpOnly: true,
         path: "/",
       });
-      ctx.cookies.set("game-languages", languages.join("|"), {
+      ctx.cookies.set(langaugesCookie, languages.join("|"), {
         httpOnly: true,
         path: "/",
       });
+    },
+  }),
+  getPreferences: defineAction({
+    handler: (_, ctx) => {
+      const difficulty = ctx.cookies.get(difficultyCookie)?.value;
+      if (
+        difficulty !== "pemula" &&
+        difficulty !== "menengah" &&
+        difficulty !== "mahir"
+      ) {
+        throw new ActionError({
+          message: "Tingkat kesulitan tidak valid!",
+          code: "BAD_REQUEST",
+        });
+      }
+
+      const languages = ctx.cookies.get(langaugesCookie)?.value.split("|");
+      if (languages === undefined || languages.length === 0) {
+        throw new ActionError({
+          message: "Bahasa tidak valid!",
+          code: "BAD_REQUEST",
+        });
+      }
+
+      return { difficulty: <Difficulty>difficulty, languages };
     },
   }),
 };
