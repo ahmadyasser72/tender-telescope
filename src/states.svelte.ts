83d7feb91c7df1a8dbcf8f3621e8bc5b72ef97fe
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-12-02 21:44:55 +0800

    simpan gameState di localStorage

--- src/states.svelte.ts
+++ src/states.svelte.ts
@@ -1,7 +1,23 @@
 import type { GameState } from "./types";
+import { isBrowser } from "./utils";
 
-export const gameState = $state<GameState>({
+const STORAGE_KEY = "game-state-v1";
+
+let initialState: GameState = {
   volume: 100,
   difficulty: undefined,
   languages: [],
-});
+};
+
+if (isBrowser) {
+  const localState = localStorage.getItem(STORAGE_KEY);
+  if (localState) initialState = JSON.parse(localState);
+}
+
+export const gameState = $state<GameState>(initialState);
+
+if (isBrowser) {
+  $effect.root(() => {
+    $effect(() => localStorage.setItem(STORAGE_KEY, JSON.stringify(gameState)));
+  });
+}
