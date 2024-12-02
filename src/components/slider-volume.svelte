d3402e6b88d31b12019d38409da3768458db1415
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-12-02 20:09:44 +0800

    putar suara beep saat mengubah slider

--- src/components/slider-volume.svelte
+++ src/components/slider-volume.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import beepMP3 from "$lib/assets/beep.mp3";
   import { Slider } from "$lib/components/ui/slider";
   import { Toggle } from "$lib/components/ui/toggle";
+  import { isBrowser } from "$lib/utils";
 
   import { HeadphoneOff, Headphones } from "lucide-svelte";
 
@@ -13,11 +15,21 @@
   let volumeBeforeMuted = $state(0);
   let muted = $state(volume === 0);
 
+  let beep: HTMLAudioElement;
+  if (isBrowser) beep = new Audio(beepMP3);
+
+  const forcePlayBeep = () => {
+    beep.pause();
+    beep.currentTime = 0;
+    beep.play();
+  };
+
   const onMuteToggle = () => {
     if (muted) {
       volumeBeforeMuted = volume;
       volume = 0;
     } else {
+      forcePlayBeep();
       volume = volumeBeforeMuted > 0 ? volumeBeforeMuted : 100;
     }
   };
@@ -41,6 +53,7 @@
   <Slider
     value={[volume]}
     onValueChange={(value) => (muted = (volume = value[0]) === 0)}
+    onValueCommit={forcePlayBeep}
     min={0}
     max={100}
     step={5}
